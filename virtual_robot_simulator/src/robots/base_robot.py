70fbfc4a9e81b78d8541de6218753160071ac924
Samitrad <159942333+Samitrad@users.noreply.github.com>
2024-07-16 01:51:09 +0300

    adding getters and setters

--- virtual_robot_simulator/src/robots/base_robot.py
+++ virtual_robot_simulator/src/robots/base_robot.py
@@ -10,6 +10,33 @@ class Robot:
 
     def work(self):
         pass
+    def work(self):
+        pass
+    
+    
+    def name(self) -> str:
+        return self._name
+
+    
+    def name(self, new_name: str):
+        self._name = new_name
+
+  
+    def batterylvl(self) -> int:
+        return self._batterylvl
+
+    
+    def batterylvl(self, new_batterylvl: int):
+        self._batterylvl = new_batterylvl
+
+  
+    def status(self) -> str:
+        return self._status
+
+    
+    def status(self, new_status: str):
+        self._status = new_status
+        
     
     def check_battery(cls, batterylvl: int) -> str:
         """Class method to check battery level"""
